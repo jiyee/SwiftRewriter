984a5c55a06dd953227cc471e192e542cb639eaa
jiyee <jiyee.sheng@gmail.com>
2024-09-20 18:04:19 +0800

    comment

--- Tests/Core/TypeSystemTests/CodeDefinition/CodeDefinitionTests.swift
+++ Tests/Core/TypeSystemTests/CodeDefinition/CodeDefinitionTests.swift
@@ -5,6 +5,7 @@ import TypeSystem
 @testable import TypeSystem
 
 class CodeDefinitionTests: XCTestCase {
+    // NOTE: - 测试可变参数函数的参数定义
     func testVariadicFunctionParameterDefinition() {
         let signature = FunctionSignature(
             name: "a",
@@ -19,6 +20,26 @@ class CodeDefinitionTests: XCTestCase {
             .forLocalIdentifier(
                 "b",
                 type: .array(.int),
+                isConstant: true,
+                location: .parameter(index: 0)
+            )
+        ])
+    }
+
+    func testFunctionParameterDefinition() {
+        let signature = FunctionSignature(
+            name: "a",
+            parameters: [
+                .init(name: "b", type: .int),
+            ]
+        )
+
+        let result = CodeDefinition.forParameters(inSignature: signature)
+
+        XCTAssertEqual(result, [
+            .forLocalIdentifier(
+                "b",
+                type: .int,
                 isConstant: true,
                 location: .parameter(index: 0)
             )
